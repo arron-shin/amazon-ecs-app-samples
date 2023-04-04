a49034c328400b7ef37a9c3894aa3f6dfe308503
Shin <jungseob@bcd0742be704.ant.amazon.com>
2023-04-04 23:07:33 +0900

    Add initial codes

---
+++ cats-api/routes/api.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var os = require('os');
+
+/* GET cats container IP. */
+router.get('/container-ip', function(req, res, next) {
+  const networkInterfaces = os.networkInterfaces();
+  const serverIP = networkInterfaces.eth0[0].address;
+  res.send(`Server IP address: ${serverIP}`);
+});
+
+/* GET cats random image. */
+router.get('/image', function(req, res, next) {
+  var imgArrayUrls = new Array();
+  var bucket = "https://ecs-demogo-pictures.s3.ap-northeast-2.amazonaws.com/cat/"
+  var ext = ".jpeg"
+  for(i=0;i<10;i++){
+    imgArrayUrls[i] = (bucket+i+ext);
+  }
+  const randomimgArrayUrl = imgArrayUrls[Math.floor(Math.random() * imgArrayUrls.length)];
+  res.send(randomimgArrayUrl);
+});
+
+module.exports = router;
