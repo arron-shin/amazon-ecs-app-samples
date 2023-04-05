58177b7f24ce76fd7759f297fbd0400a0e644d93
Shin <jungseob@bcd0742be704.ant.amazon.com>
2023-04-05 10:15:28 +0900

    ref: web, cats-api, dogs-api

--- cats-api/routes/api.js
+++ cats-api/routes/api.js
@@ -1,24 +1,21 @@
-var express = require('express');
-var router = express.Router();
-var os = require('os');
+const express = require('express');
+const router = express.Router();
+const os = require('os');
 
-/* GET cats container IP. */
-router.get('/container-ip', function(req, res, next) {
-  const networkInterfaces = os.networkInterfaces();
-  const serverIP = networkInterfaces.eth0[0].address;
+router.get('/container-ip', (req, res) => {
+  const serverIP = os.networkInterfaces().eth0[0].address;
   res.send(`Server IP address: ${serverIP}`);
 });
 
-/* GET cats random image. */
-router.get('/image', function(req, res, next) {
-  var imgArrayUrls = new Array();
-  var bucket = "https://ecs-demogo-pictures.s3.ap-northeast-2.amazonaws.com/cat/"
-  var ext = ".jpeg"
-  for(i=0;i<10;i++){
-    imgArrayUrls[i] = (bucket+i+ext);
-  }
+router.get('/image', (req, res) => {
+  const imgArrayUrls = Array.from({ length: 10 }, (_, i) => `https://ecs-demogo-pictures.s3.ap-northeast-2.amazonaws.com/cats/${i}.jpeg`);
   const randomimgArrayUrl = imgArrayUrls[Math.floor(Math.random() * imgArrayUrls.length)];
-  res.send(randomimgArrayUrl);
+  const serverIP = os.networkInterfaces().eth0[0].address;
+  const responseData = {
+    imageUrl: randomimgArrayUrl,
+    containerIp: serverIP,
+  };
+  res.json(responseData);
 });
 
 module.exports = router;
